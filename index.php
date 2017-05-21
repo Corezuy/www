cb4e87eedfb35c1e14e9a162c48cd7adb683e649
Corezuy <grandex17@gmail.com>
2017-05-20 23:58:45 -0400

    display news

--- index.php
+++ index.php
@@ -5,6 +5,21 @@ require"header.php";
 
 <p>getting started</p>
 
+<?php
+					
+$newlim = $SETTINGS['Number_of_News_Items'];
+$newsformat = html_entity_decode( $SETTINGS['News_Template'] );
+$result = $db->query( "SELECT * FROM news ORDER BY ID DESC LIMIT $newlim" );
+
+while ( $news = $db->fetch( $result ) ) {
+    $newdisplay = str_replace( "[date]", $news['Date'], $newsformat );
+    $newdisplay = str_replace( "[title]", $news['Title'], $newdisplay );
+    $newdisplay = str_replace( "[body]", html_entity_decode( $news['Body'] ), $newdisplay );
+    echo $newdisplay;
+} 
+
+?>
+
 <hr>
 
 <?php
