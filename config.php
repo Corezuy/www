abfc66ecf7c12ad164dd9b6a8e3b7b7c6e49605b
Corezuy <grandex17@gmail.com>
2017-05-21 00:37:00 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 //settings
-$DB_HOST 			= '';
-$DB_USER		= '';
+$DB_HOST 			= 'localhost';
+$DB_USER		= 'root';
 $DB_PASS		= '';
-$DB_NAME			= '';
+$DB_NAME			= 'db';
 
 //open mysql connection
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
